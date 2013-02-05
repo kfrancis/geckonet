70520c3f5932b3574b83c4a8fb0582e3051987e6
Kori Francis <byter@me.com>
2013-02-05 11:02:00 -0500

    Initial commit

---
+++ Source/Geckonet.Core/Models/GeckoModels.cs
@@ -0,0 +1,78 @@
+﻿#region License, Terms and Conditions
+//
+// GeckoModels.cs
+//
+// Authors: Kori Francis <twitter.com/djbyter>
+// Copyright (C) 2013 Kori Francis. All rights reserved.
+// 
+//  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW:
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+#endregion
+
+namespace Geckonet.Core.Models
+{
+    #region Imports
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml.Serialization;
+    #endregion
+
+    /// <summary>
+    /// Response consists of two values. First value is main to be displayed, the second value is used to work out the % change as a secondary stat.
+    /// See <a href="http://docs.geckoboard.com/custom-widgets/number.html">documentation</a>.
+    /// </summary>
+    [DataContract(Name = "root", Namespace = ""), XmlRoot("root", Namespace = "")]
+    public class NumberAndSecondaryStat
+    {
+        /// <summary>
+        /// Optional. Setting this to 'true' will show the numerical difference not the percent difference.
+        /// </summary>
+        [DataMember(Name = "absolute", IsRequired = false), XmlElement("absolute")]
+        public bool? Absolute { get; set; }
+
+        [DataMember(Name = "type", IsRequired = false), XmlElement("type")]
+        public string Type { get; set; }
+
+        public DataItem First { get; set; }
+        public DataItem Second { get; set; }
+    }
+
+    [DataContract(Name = "item", Namespace = ""), XmlType("item", Namespace = "")]
+    public class DataItem
+    {
+        /// <summary>
+        /// The label of this data point
+        /// </summary>
+        [Required, DataMember(Name = "text", IsRequired = true), XmlElement("text")]
+        public string text { get; set; }
+
+        /// <summary>
+        /// The value of this data point
+        /// </summary>
+        [Required, DataMember(Name = "value", IsRequired = true), XmlElement("value")]
+        public decimal @value { get; set; }
+    }
+}
