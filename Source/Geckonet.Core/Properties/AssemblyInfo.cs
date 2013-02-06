0e5b72d3566c5e2d0f0dcf25789d6d72281fbc6e
Kori Francis <byter@me.com>
2013-02-06 09:58:25 -0500

    Fix nuget spec

--- Source/Geckonet.Core/Properties/AssemblyInfo.cs
+++ Source/Geckonet.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Geckonet.Core")]
+[assembly: AssemblyTitle("geckonet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kori Francis")]
-[assembly: AssemblyProduct("Geckonet.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("geckonet")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
